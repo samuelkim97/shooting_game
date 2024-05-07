0648475623e981e5f47fd7b19a6bd6f3130c4d73
BumSoo Kim <kbeom00@gmail.com>
2024-05-07 23:41:25 +0900

    done.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pygame, sys
+import pygame, sys, random
+
 
 
 
@@ -8,10 +9,22 @@ class Crosshair(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load(img_path)
         self.rect = self.image.get_rect() # create rect to image
+        self.gunshot = pygame.mixer.Sound("./assets/gunshot_6.wav") # load wav files for gunshot
 
+    def shoot(self):
+        self.gunshot.play() # function that play sounds
+        pygame.sprite.spritecollide(crosshair, target_group, True)
     def update(self):
         self.rect.center = pygame.mouse.get_pos()
 
+
+# target class
+class Target(pygame.sprite.Sprite):
+    def __init__(self, img_path, pos_x, pos_y):
+        super().__init__()
+        self.image = pygame.image.load(img_path)
+        self.rect = self.image.get_rect()
+        self.rect.center = [pos_x, pos_y]
 
 
 # general setup
@@ -27,12 +40,16 @@ background = pygame.transform.scale(background, (screen_width, screen_height))
 pygame.mouse.set_visible(False) # invisible mouse
 
 
-
-
 # CROSSHAIR
 crosshair = Crosshair("./assets/crosshair_outline_small.png") # create object
 crosshair_group = pygame.sprite.Group() # group objects
 crosshair_group.add(crosshair) # added to group 
+
+# TARGET
+target_group = pygame.sprite.Group()
+for target in range(20):
+    new_target = Target("./assets/target_red1.png", random.randrange(0, screen_width), random.randrange(0, screen_height))
+    target_group.add(new_target)
 
 
 # main loop
@@ -41,9 +58,12 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            crosshair.shoot() # gun shot sound when mouse click
 
     pygame.display.flip()
-    screen.blit(background, (0, 0))
-    crosshair_group.draw(screen)
+    screen.blit(background, (0, 0)) # background
+    target_group.draw(screen)
+    crosshair_group.draw(screen) # crosshair
     crosshair_group.update()
     clock.tick(60)
